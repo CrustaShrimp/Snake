bfde78bbe575eb31cd6095832330bb2452df012a
Shrimp <jagjonkman@gmail.com>
2020-02-06 22:36:50 +0100

    Added m_iNumber to GridElement to use for sorting in the set

--- GridElement.cpp
+++ GridElement.cpp
@@ -3,6 +3,7 @@
 GridElement::GridElement()
 	:m_iX(0)
 	,m_iY(0)
+	,m_iNumber(0)
 	,m_bActive(false)
 {
 
@@ -11,6 +12,16 @@ GridElement::GridElement()
 GridElement::GridElement(const int iX, const int iY)
 	:m_iX(iX % g_iGridSizePlusOne)
 	,m_iY(iY % g_iGridSizePlusOne)
+	,m_iNumber(0)
+	,m_bActive(true)
+{
+
+}
+
+GridElement::GridElement(const int iX, const int iY, const int iNumber)
+	:m_iX(iX% g_iGridSizePlusOne)
+	,m_iY(iY% g_iGridSizePlusOne)
+	,m_iNumber(iNumber)
 	,m_bActive(true)
 {
 
--- GridElement.h
+++ GridElement.h
@@ -7,9 +7,12 @@ extern const int g_iGridSizePlusOne;
 class GridElement
 {
 public:
-	GridElement();								// Default constructor
-	GridElement(const int iX, const int iY);	// Constructor	
-	~GridElement();								// Destructor
+
+	// TODO comments
+	GridElement();													// Default constructor
+	GridElement(const int iX, const int iY);						// Constructor	
+	GridElement(const int iX, const int iY, const int iNumber)		// Constructor
+	~GridElement();													// Destructor
 private:
 	int		m_iX;			// X coordinate of the element
 	int		m_iY;			// Y coordinate of the element
