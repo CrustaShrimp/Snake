ea11e40d96f7247c06428e938bf17a42b6297e89
Shrimp <jagjonkman@gmail.com>
2020-12-04 16:23:16 +0100

    Solve #58: Change the game over dialog name to Game Over Also move the GAME OVER and score upwards, to make space for the highscore stuff

---
+++ resource.h
@@ -0,0 +1,63 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Snake.rc
+//
+#define IDC_MYICON                      2
+#define IDD_SNAKE_DIALOG                102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDC_SNAKE                       109
+#define IDR_MAINFRAME                   128
+#define IDD_GAMEOVER                    129
+#define IDD_START                       132
+#define IDI_ROUNDSNAKE                  134
+#define IDI_BLOCKSNAKE                  135
+#define IDI_BWSNAKE                     136
+#define IDA_SNAKE                       137
+#define IDA_SNAKEJAZZ                   137
+#define IDD_HELP                        138
+#define IDI_ICON1                       142
+#define IDS_TIMER                       543
+#define IDC_GAMEOVER                    1001
+#define IDC_YOURSCORE                   1002
+#define IDC_SCORE                       1003
+#define IDC_SETEASY                     1003
+#define IDC_SETMEDIUM                   1004
+#define IDC_SETHARD                     1005
+#define IDSTART                         1007
+#define IDRESTART                       1008
+#define IDC_SOUND                       1009
+#define IDC_STATICSTART2                1010
+#define IDC_STATICTEXT3                 1011
+#define IDC_STATICSTART3                1011
+#define IDC_STATICSTART1                1012
+#define IDC_STATICABOUT1                1013
+#define IDC_STATICABOUT2                1014
+#define IDC_STATICABOUT3                1015
+#define IDCLS                           1017
+#define IDC_STATICINFO2                 1019
+#define IDC_STATICINFO3                 1020
+#define IDM_DIFFICULTY_EASY             32778
+#define IDM_DIFFICULTY_MEDIUM           32779
+#define IDM_DIFFICULTY_HARD             32780
+#define IDM_RESTART                     32782
+#define ID_OPTIONS_SOUND                32783
+#define IDM_OPTIONS_SOUND               32784
+#define ID_HELP_HELP                    32785
+#define IDM_HELP                        32786
+#define IDI_ICONRS                      -1
+#define IDC_STATICINFO1                 -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        143
+#define _APS_NEXT_COMMAND_VALUE         32787
+#define _APS_NEXT_CONTROL_VALUE         1022
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
